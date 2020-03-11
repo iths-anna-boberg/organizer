9c29ef27e94359f5e937cdf77d7930564d298183
iths-anna-boberg <amboberg@gmail.com>
2020-03-11 15:09:49 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,24 +4,53 @@ import Controller from "./controller.js"
 
 const app = new Controller(new Model(), new View());
 
-// let myList = app.model.addListItem("Fyra")
-// console.log(app.model.organizerList)
+//** LÄGG TILL NY LISTA **
+// let myList = app.model.addListItem("Städa")
+//** SLUT LÄGG TILL NY LISTA **
 
 // console.log(app.model.getListItem(1582121111184))
 
-//---------------
-// let hey = app.model.createCard("Lite text");
+//--------------- LÄGG TILL KORT PÅ LISTA
+// let card = app.model.createCard("Köket");
+// let id = 1582201663045;
+// app.model.addCardToListItem(id, card)
+//--------------- SLUT LÄGG TILL KORT PÅ LISTA
 
-// let object = app.model.getListItem(1582121803240)
-//-----------------
+//********* TESTA DELETE (LISTA)
+// app.model.deleteListItem(1582139053791);
+//********* SLUT DELETE
 
-// app.model.addCardToListItem(object, hey)
+//---- *DELETE CARD* ----
+// app.model.deleteCard(1582192576864, 1582192690109)
+// ---- *SLUT DELETE CARD* ----
 
-// console.log(hey)
-// let id = 1582120717149;
+// ---------- Get CARD
+// app.model.getCard
 
-// app.model.addCardToListItem(id, hey)
 
+//------------ testa byta namn------------------
+
+// app.model.organizerList[0].cards[2].title = "Kanelbullar"
+// app.model.save();
+
+// app.model.organizerList[0].name = "Titta på";
+// app.model.save();
+
+// app.model.changeListName(1582192570553, "Serier")
+
+//------------ slut testa byta namn---------------
+
+// console.log(app.model.getCard(1582192580161, 1582201710217))
+
+
+// Rendera listor och deras cards och eventlyssnare
+app.view.displayLists(app.model.organizerList);
+app.view.dragEventListeners();
+
+
+
+
+// console.log(app.model.organizerList)
 
 
 
